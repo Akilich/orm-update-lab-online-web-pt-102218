72bd784e9caa1f664234c9f0b0ef8f73eeb23b1b
Akili Humphrey <akilihumphrey@gmail.com>
2019-01-30 08:04:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,12 +43,9 @@ class Student
     student
   end
   
-  def self.new_from_db(id=nil, name, grade)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student 
+  def self.new_from_db(row)
+    new_student = self.new(row[1], row[2], row[0])
+    new_student
   end
   
   def self.find_by_name(name)

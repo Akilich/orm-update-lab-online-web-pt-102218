cbdf1041032a7ec917e12905df0889007893d97b
Akili Humphrey <akilihumphrey@gmail.com>
2019-01-30 08:09:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,12 +51,8 @@ class Student
 
    def self.find_by_name(name)
     sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE name = ?
-      LIMIT 1
+      SELECT * FROM students WHERE name = ? LIMIT 1
     SQL
-
      DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
@@ -64,11 +60,8 @@ class Student
 
    def update
     sql = <<-SQL
-      UPDATE students
-      SET name = ?, grade = ?
-      WHERE id = ?
+      UPDATE students SET name = ?, grade = ? WHERE id = ?
     SQL
-
      DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 end
